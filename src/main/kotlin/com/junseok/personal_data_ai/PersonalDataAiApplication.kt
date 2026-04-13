590f64a1e3347aa6a258d382ec08228923d401b9
newtonjjang <newtonjjang@naver.com>
2026-04-13 22:35:44 +0900

    feat: project initialization

---
+++ src/main/kotlin/com/junseok/personal_data_ai/PersonalDataAiApplication.kt
@@ -0,0 +1,11 @@
+package com.junseok.personal_data_ai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PersonalDataAiApplication
+
+fun main(args: Array<String>) {
+	runApplication<PersonalDataAiApplication>(*args)
+}
---
+++ src/test/kotlin/com/junseok/personal_data_ai/PersonalDataAiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.junseok.personal_data_ai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PersonalDataAiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
